7747e0b972be58bfafb89328547f8d93bebb275e
Sai Lalasa Challa <lalasa.challa22@gmail.com>
2025-07-03 11:13:59 +0530

    Update leapyear.java

--- leapyear.java
+++ leapyear.java
@@ -4,10 +4,10 @@ public class Main{
   public static void main(String args[]){
     Scanner sc= new Scanner(System.in);
     int year= sc.nextInt();
-    if(year%4==0 || year%400==0){
+    if((year%4==0 && year%100!=0) || (year%400==0)){
       System.out.println("Schedule Event");
     }
-    else{
+    else {
       System.out.println("No Event This Year");
     }
   }
