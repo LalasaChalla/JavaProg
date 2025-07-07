d43f953d9d7e59d3722c4a2c8725d62b2f0694b0
Sai Lalasa Challa <lalasa.challa22@gmail.com>
2025-07-07 10:14:06 +0530

    Create apgp.java
    Virus Growth Rate Prediction Based on Daily Increases
    
    In a research lab, a group of scientists is studying the growth rate of a new virus that is spreading across the population. The scientists are tracking the virus's growth over several days, and the virus's growth rate changes every day in a predictable pattern. The growth rate on the first day increases by 13, on the second day it increases by twice 13, on the third day it increases by three times 13, and so on. They need to predict the virus growth for the next 'n' days.
    
    Your task is to write a program to predict the growth rate for the virus over the first 'n' days, where each day's growth follows the pattern described above.
    
    You are given a number 'n' which represents the number of days (or terms in the series). You need to generate the growth rate for the virus for the first 'n' days. The growth rate starts with a value of 2 on the first day, and the increase on each subsequent day is based on the multiples of 13 as described.
    
    
    
    Input Format
    
    The input consists of a single integer 'n', which denotes the number of terms (or days) to be printed in the series.
    
    
    Constraints
    
    'n' is a positive integer (NA for any additional constraints).
    
    
    Output Format
    
    Print the series of virus growth rates for 'n' days, separated by spaces.

---
+++ apgp.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+  public static void main(String []args){
+    Scanner sc= new Scanner(System.in);
+    int n=sc.nextInt();
+    int a=2;
+    System.out.print(a+" ");
+    for(int i=1;i<n;i++){
+      a=a+13*i;
+      System.out.print(a+" ");
+    }
+  }
+}
